2af2a64b78ead18c384127fbac8275ab1c4597b6
mdumar112233 <mdumarfaruk187326@gmail.com>
2021-03-25 11:18:52 +0600

    post data from body

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
+
 // const  rootCall = (req, res) => res.send('thank you');
 
+const users = ['uamr', 'faruk', 'rofik', 'rafi'];
+
 app.get('/', (req, res) => {
-    res.send('thank you i am here');
+    const fruit = {
+        product: 'apple'
+    }
+    res.send(fruit);
 });
+
+
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    console.log(req.query);
+    const name = users[userId];
+    res.send({userId, name})
+})
+
+// post
+app.post('/addUser', (req, res) => {
+    // save to database
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
+})
+
 
 app.listen(3000, () => console.log('listening to port 3000'));
 
